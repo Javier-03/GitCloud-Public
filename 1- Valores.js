1bb9f2b414f8218a6f0aeb6093a7f5f199feef81
Javier <95238615+Javier-03@users.noreply.github.com>
2022-02-18 22:18:09 -0500

    Add files via upload

---
+++ 1- Valores.js
@@ -0,0 +1,30 @@
+                      /Tipos de Valores/
+
+/"VALORES PRIMITIVOS"/
+
+-("number")- "Almacenar números";
+- 40
+/
+
+-("string")- "Almacenar texto, con comillas"
+- "Javier Gómez"
+/
+
+-("Boolean")- "Almacena valores lógicos"
+-true / (1)
+-false / (0)
+/
+
+-("Valores Vacios")
+-null- "Cuando el dato no exite"
+-undefined- "Cuando no se asigna un valor a variable"
+
+
+                    /"VALORES NO PRIMITIVOS"/ - "object"
+
+-("arrays")- "Usa corchetes"
+-[1,2,3]
+/
+
+-("Valores Objeto")- "Usa llaves"
+-{nombre: "Javier"}
---
+++ 2- Variables.js
@@ -0,0 +1,23 @@
+var nombre = "Javier";
+
+
+"Estados de una variable"
+
+//declarar
+var edad;
+edad
+
+//inicializar
+edad = 30;
+edad
+
+var elemetos = ["Computadora", "Celular"];
+elemetos
+
+var personas = {
+    nombre: "Juan",
+    edad: 50
+}
+personas;
+
+
