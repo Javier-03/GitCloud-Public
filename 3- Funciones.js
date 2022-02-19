1bbcbe163c0d6397801207820ee66864129ee413
Javier <95238615+Javier-03@users.noreply.github.com>
2022-02-18 22:15:59 -0500

    Add files via upload

---
+++ 3- Funciones.js
@@ -0,0 +1,27 @@
+//?Funciones
+
+"Son tareas que se llevan a cabo en el navegador web"
+"Hacer pasos, calculos o generar algo"
+//function=palabra reservada
+"Alt + 96 = (``)"
+
+(Ejemplos)
+"Hay 2 tipos de funciones"
+
+//!Declarativas
+//function son los valores que se guardan
+function mifuncion() {
+    return 3; //mi function va a guardar el número
+}
+mifuncion();
+
+
+
+//!Expresión () Funciones anonimas
+//variable que guarda una function
+var mifuncion = function (a,b){ //no se están declarando variables
+    return a + b;
+}
+//Parametros que estoy esperando recibir
+mifuncion();
+
